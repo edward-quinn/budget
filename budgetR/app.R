297dd0327c248d1a1d58d138db32903a28428e2d
edward-quinn <edwardquinn@ufl.edu>
2023-09-09 15:43:23 -0400

    first draft of app

--- budgetR/app.R
+++ budgetR/app.R
@@ -5,9 +5,87 @@
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
-# Test text
+# 
+
+
 
 library(shiny)
+library(tidyverse)
+library(janitor)
+
+
+# Data preparation
+
+df <- read_csv("budget.csv",
+               name_repair = make_clean_names,
+               col_types = c("ccccnncnc"),
+               col_select = c(date, category, tags, expense_amount)) |> 
+  mutate(date = mdy(date),
+         month = month(date,
+                       label = TRUE,
+                       abbr = FALSE))
+
+
+months <- unique(df$month)
+
+# tester filtering
+
+df |> 
+  filter(month == "August") |> 
+  group_by(category) |> 
+  summarise(total_expense = sum(expense_amount)) |> 
+  arrange(desc(total_expense))
+
+
+
+# User Interface
+ui <- fluidPage(
+  
+  titlePanel("Medward Finance"),
+  
+  fluidRow(
+    column(2,
+           selectInput("month", "Month", choices = months)
+    )
+  ),
+  
+  fluidRow(
+    column(4, tableOutput("expenses")),
+    column(8, plotOutput("expenses_plotted"))
+  )
+)
+
+
+# Server
+server <- function(input, output, session) {
+  
+  selected <- reactive({
+    df |> 
+    filter(month == input$month) |> 
+    group_by(category) |> 
+    summarise(Expenses = sum(expense_amount)) |> 
+    arrange(desc(Expenses))
+  })
+  
+  output$expenses <- renderTable(
+    selected() 
+  )
+  
+  output$expenses_plotted <- renderPlot({
+    selected() |> 
+      ggplot(aes(x=category, y=Expenses)) +
+      geom_bar(stat = "identity") +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 90)) +
+      ylim(0,1400) +
+      xlab("Spending Category") +
+      ylab("Cost")
+  }, res = 96)
+}
+
+# Run App
+shinyApp(ui, server)
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
